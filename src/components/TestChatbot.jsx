e403d3727094fcd29fc68cf90b739c9e9f5724eb
iagoalvarezsande <iago.alvarez.sande@gmail.com>
2023-11-25 01:25:47 +0000

    a

--- src/components/TestChatbot.jsx
+++ src/components/TestChatbot.jsx
@@ -4,7 +4,6 @@ import Stack from 'react-stackai';
 const TestChatbot = () => {
   return (
     <div>
-        <h1>TestChatbot</h1>
         <Stack project= "https://www.stack-ai.com/embed/0cbdd5c3-0bc4-4b7c-b7d7-7dad0c0905b4/18fcc182-a1d3-4eb3-8087-2d681129dca3/6560b9d7dfa2ba13679ad0d3" />
 
     </div>
